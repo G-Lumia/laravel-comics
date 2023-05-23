02f82039e86e32918a3d4364b4f425be25f68538
G-Lumia <giorgiolumia@gmail.com>
2023-05-23 11:18:48 +0200

    fixed style and upgraded visual interface

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <h1>Comics</h1>
         <div class="row">
             @foreach ($comics as $comic)
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-3">
                 <a href="{{ route('comics.show', ['id' => $loop->index]) }}">
                     <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                 </a>
